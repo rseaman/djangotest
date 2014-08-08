0682c14174f99c6e54e310cf477b87f0399f1309
Riley Seaman <riley.seaman@gmail.com>
2014-08-08 15:34:57 -0500

    Chapter 4 done

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
+from django.http      import HttpResponse
+from django.template  import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango says hello world! <a href='/rango/about/'>About</a>")
+    # Get context of request
+    context = RequestContext(request)
+
+    # Dictionary to be passed to template engine
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    # Return rendered response
+    return render_to_response('rango/index.html', context_dict, context)
+
 
 def about(request):
     return HttpResponse("Rango says: Here is the about page. <a href='/rango/'>Index</a>")
